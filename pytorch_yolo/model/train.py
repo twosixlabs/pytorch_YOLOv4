59e2bbd66a78834888c4c5dd394fb3b005de9724
jcronay <jennifer.csicsery-ronay@twosixlabs.com>
2024-01-15 15:18:36 -0600

    path fix

--- pytorch_yolo/model/train.py
+++ pytorch_yolo/model/train.py
@@ -28,7 +28,7 @@ from torch.nn import functional as F
 from tensorboardX import SummaryWriter
 from easydict import EasyDict as edict
 
-from dataset import Yolo_dataset
+from model.dataset import Yolo_dataset
 from cfg import Cfg
 from models import Yolov4
 from tool.darknet2pytorch import Darknet
